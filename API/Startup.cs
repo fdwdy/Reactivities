891f1ce980a9854ea8e25f4fdb9eabd2a1d640a1
Fedor Saevich <f.saevich@gmail.com>
2020-09-02 21:21:27 +0300

    + Add Client Project

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -41,6 +47,8 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             //app.UseHttpsRedirection();
 
